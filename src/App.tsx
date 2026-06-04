4c5a52c97601f8381700a2e263fe3592c7a512e6
gallantgibran <gallantgibran024@gmail.com>
2026-06-04 20:25:37 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-import { Button } from "@/components/ui/button"
+import ToDoListCard from "./components/cards/ToDoListCard"
+import Header from "./components/header/Header"
 
 export function App() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
+    <main>
+      <Header />
+      <div className="relative -top-12 z-30 container mx-auto max-w-135.25">
+        <ToDoListCard />
       </div>
-    </div>
+    </main>
   )
 }
 
